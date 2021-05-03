e7a1ee403a5b49ce7d93ab9f5775475cdd018e39
tszabo75 <tszabo75@users.noreply.github.com>
2021-05-03 15:07:59 +0200

    Update Actions.py

--- Actions.py
+++ Actions.py
@@ -2,7 +2,7 @@
 
 #Probes the connectivity to given address-port-tuple via connection type protocol and returns elapsed time. If a protocol other than https or tls is given, function probes a simple tcp socket connection.
 def Actions():
-	print("Hello world")
+	print("Hello world!")
 
 if __name__ == '__main__':
 	Actions()
